4935a07c5cee888ce259e56ee118a4aa3ebec07c
Emily Cwienk <emily.cwienk@nueprice.com>
2022-05-10 10:21:09 +0200

    Initial commit.

--- main.c
+++ main.c
@@ -1,24 +1,52 @@
-/**
- *
- *
- *
- **/
+/*
+    Tag des Jahres
+    Autor: Emily Cwienk
+    Erstellt am: 29.03.2022
+*/
 
 #include <stdio.h>
-#include <stdlib.h>
 
-/**
- *
- *
- **/
-int day_of_the_year(int day, int month, int year)
-{
+int leapyear(year){
+
+    if (year % 400 == 0) {
+    printf("Jahr ist ein Schaltjahr"); // tag pro monat [2] = 29
+    
+
+    } else if (year % 4 == 0) {
+        printf ("Jahr ist ein Schaltjahr"); // tag pro monat [2] = 29
+    }
 
     return 0;
 }
 
-int main()
+int main(){
+
+int days_per_month[12] = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+int year = 0;
+printf("Jahr:");
+scanf("%i", &year);
+
+int month = 0;
+printf("Monat:");
+scanf("%i", &month);
+
+int day = 0;
+printf("Tag:");
+scanf("%i", &day);
+
+int result = 0;
+
+leapyear(year);
+
+for (int i = 0; i < month - 1; i++)
 {
-    printf("Tag des Jahres: %i\n", day_of_the_year(31, 12, 2018));
-    return 0;
+    result += days_per_month[i];
+}
+
+result += day;
+
+printf("Das Datum '%i. %i. %i' ist der %i. Tag des Jahres", day, month, year, result);
+
+
 }
